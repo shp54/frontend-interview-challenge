094e35941de8947894f2a9ad4504c46aff0230bf
Shameek Poddar <shameek.poddar@gmail.com>
2017-11-05 10:27:17 -0500

    Clean up moment and duration logic a little

--- challenges/3. React Components/challenge.js
+++ challenges/3. React Components/challenge.js
@@ -20,15 +20,15 @@ export class TimeAvailabilityPills extends React.Component {
     let freeTimes = findFreeTimes(events[0].start, events[events.length - 1].end, duration, events);
     let getAvailability = (time) => freeTimes.filter((free) => moment(time).isBetween(free.start, free.end)).length > 0;
     let increments = moment(end).diff(moment(start), 'minutes') / duration;
-    let timeBlocks = Array(increments).fill(0).map((x, i) => x + i)
-                     .map((i) => moment(start).clone().add(i * duration, 'minutes'))
-                     .map((time) => ({ time: time, isAvailable: getAvailability(time)}));
+    let timeBlocks = Array.from(new Array(increments), (x, i) => i * duration)
+                     .map((mins) => moment(start).clone().add(mins, 'minutes'))
+                     .map((time) => ({ time: time.format('h:mm'), isAvailable: getAvailability(time)}));
     
     return (
       <div className='time-availability-pills'>
         {timeBlocks.map((block, index) => (
           <Pill key={index} className={block.isAvailable ? 'available' : 'unavailable'}>
-            {block.time.format('h:mm')}
+            {block.time}
           </Pill>
         ))}
       </div>
