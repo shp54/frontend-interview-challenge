7a2eb008615c0dabbae299513a418c2f6e8f0ed5
Shameek Poddar <shameek.poddar@gmail.com>
2017-11-05 21:43:23 -0500

    Move render logic to selectors using reselect

--- challenges/3. React Components/challenge.js
+++ challenges/3. React Components/challenge.js
@@ -1,6 +1,36 @@
 import React from 'react';
 import moment from 'moment';
-import { findFreeTimes } from '../1. Meeting Availability/challenge'
+import { createSelector } from 'reselect';
+import { findFreeTimes } from '../1. Meeting Availability/challenge';
+
+const eventsSelector = props => props.events,
+      durationSelector = props => props.duration, 
+      startSelector = props => props.start,
+      endSelector = props => props.end;
+
+const incrementsSelector = createSelector(
+    [ startSelector, endSelector, durationSelector ],
+    (start, end, duration) => moment(end).diff(moment(start), 'minutes') / duration
+  )
+
+const freeTimesSelector = createSelector(
+  [eventsSelector, durationSelector],
+  (events, duration) => {
+    let freeTimes = findFreeTimes(events[0].start, events[events.length - 1].end, duration, events)
+    return freeTimes; //Have to do this otherwise the selector returns undefined
+  })
+
+const getAvailabilityFn = (freeTimes) => (time) => freeTimes.filter((free) => moment(time).isBetween(free.start, free.end)).length > 0
+
+const timeBlocksSelector = createSelector(
+    [ incrementsSelector, startSelector, durationSelector, freeTimesSelector ],
+    (increments, start, duration, freeTimes) => {
+      const getAvailability = getAvailabilityFn(freeTimes);
+      return Array.from(new Array(increments), (x, i) => i * duration)
+                  .map((mins) => moment(start).clone().add(mins, 'minutes'))
+                  .map((time) => ({ time: time.format('h:mm'), isAvailable: getAvailability(time) }));
+     }
+  )
 
 export class TimeAvailabilityPills extends React.Component {
   static propTypes = {
@@ -9,20 +39,11 @@ export class TimeAvailabilityPills extends React.Component {
     duration: React.PropTypes.number,
     events: React.PropTypes.arrayOf(React.PropTypes.object),
   };
-
-  render() { 
-    let { start, end, duration, events } = this.props;
-        
-    let freeTimes = findFreeTimes(events[0].start, events[events.length - 1].end, duration, events);
-    let getAvailability = (time) => freeTimes.filter((free) => moment(time).isBetween(free.start, free.end)).length > 0;
-    let increments = moment(end).diff(moment(start), 'minutes') / duration;
-    let timeBlocks = Array.from(new Array(increments), (x, i) => i * duration)
-                     .map((mins) => moment(start).clone().add(mins, 'minutes'))
-                     .map((time) => ({ time: time.format('h:mm'), isAvailable: getAvailability(time) }));
-    
+  
+  render() {     
     return (
       <div className='time-availability-pills'>
-        {timeBlocks.map((block, index) => (
+        {timeBlocksSelector(this.props).map((block, index) => (
           <Pill key={index} className={block.isAvailable ? 'available' : 'unavailable'}>
             {block.time}
           </Pill>
